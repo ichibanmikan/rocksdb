39b002f5a3a6bca1f98e83fb2d8217de1464b135
ichibanmikan <ichibanmikans@gmail.com>
2023-05-04 12:20:26 +0800

    改bug

--- db/db_impl/db_impl_compaction_flush.cc
+++ db/db_impl/db_impl_compaction_flush.cc
@@ -3571,8 +3571,8 @@ Status DBImpl::BackgroundCompaction(bool* made_progress,
     TEST_SYNC_POINT_CALLBACK(
         "DBImpl::BackgroundCompaction:NonTrivial:BeforeRun", nullptr);
     // Should handle erorr?
-    compaction_job.Run().PermitUncheckedError();
-    // compaction_job.Run(ic).PermitUncheckedError();
+    // compaction_job.Run().PermitUncheckedError();
+    compaction_job.Run(ic).PermitUncheckedError();
     TEST_SYNC_POINT("DBImpl::BackgroundCompaction:NonTrivial:AfterRun");
     mutex_.Lock();
 
